0b927b84c60f285377a6ad65e68ac9b8561b82e7
Paul Day <paul@paulday.com.au>
2015-09-26 14:06:23 +1000

    update js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 var angular = require('angular');
 require('angular-flash-alert');
 require('angular-animate');
-require('./styles.less');
- 
-var moduleName = 'ErrorCatcher';
- 
-angular.module(moduleName, ['flash', 'ngAnimate'])
-    .factory('errorHttpInterceptor', ['$q', "Flash", function ($q, Flash) {
-        return {
-            responseError: function responseError(rejection) {
+require('./styles.less'); 
 
-                Flash.create('danger', rejection.data.message ? rejection.data.message : 'An error has occured.', 'customAlert');
+var moduleName = 'angular-global-alerts';
 
-                return $q.reject(rejection);
-            }
-        };
-    }])
-    .config(['$httpProvider', function($httpProvider) {
-        
-        $httpProvider.interceptors.push('errorHttpInterceptor');
+module.exports = function(style, redirect) {
 
-    }]);
+    angular.module(moduleName, ['flash', 'ngAnimate'])
+        .factory('errorHttpInterceptor', ['$q', "Flash", "$location", function ($q, Flash, $location) {
+            return {
+                responseError: function responseError(rejection) {
+                    // clean error message
+                    if(rejection.data.message.length > 200) rejection.data.message = 'An error has occured.';
 
-module.exports = moduleName;
+                    // create alert
+                    Flash.create('danger', rejection.data.message ? rejection.data.message : 'An error has occured.', style ? style : 'customAlert');
+
+                    // redirect back to redirect page
+                    $location.path(redirect ? redirect : '/');
+
+                    // return error
+                    return $q.reject(rejection);
+                }
+            };
+        }])
+        .config(['$httpProvider', function($httpProvider) {
+            
+            $httpProvider.interceptors.push('errorHttpInterceptor');
+
+        }]);
+
+        return moduleName;
+
+}
